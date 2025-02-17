b01b84db6e73b5592ff513eeb4ea61615ea74901
Sören Maschmann <smaschmann@slashwhy.de>
2025-02-17 21:33:36 +0100

    feat: add api schema & client

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,3 +1,22 @@
-interface Window {
-  Alpine: import('alpinejs').Alpine;
+import type { Client } from 'openapi-fetch';
+import type { paths as identityPaths } from '@/identity-api-schema';
+
+declare global {
+  namespace App {
+    interface Locals {
+      user: {
+        userId?: string;
+        accessToken?: string;
+        refreshToken?: string;
+        expiresAt?: string;
+      };
+      api: {
+        identity: Client<identityPaths>;
+      };
+    }
+  }
+
+  interface Window {
+    Alpine: import('alpinejs').Alpine;
+  }
 }
